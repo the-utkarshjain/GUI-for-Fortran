04ad1a8de35bd0baf68c62f969164e5dfe649f2d
Utkarsh Jain <utkarshjain@Utkarshs-MacBook-Pro.local>
2020-11-02 11:21:01 +0530

    Implemented _refresh_utility. Utkarsh Jain B17029

--- gui_func.py
+++ gui_func.py
@@ -3,6 +3,8 @@ import threading
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import hashlib
 
 
 class GUIMain(GUIBase):
@@ -12,7 +14,25 @@ class GUIMain(GUIBase):
 
     @classmethod
     def _refresh_utility(cls, first_file_path: str, second_file_path: str, third_file_path: str, memory: dict) -> bool:
-        pass
+
+        bool isupdates = False
+        checksum_file_1 = hashlib.md5(open(first_file_path).read()).hexdigest()
+        checksum_file_2 = hashlib.md5(open(second_file_path).read()).hexdigest()
+        checksum_file_3 = hashlib.md5(open(third_file_path).read()).hexdigest()
+
+        if(memory.get(first_file_path, None) != checksum_file_1):
+            memory[first_file_path] = checksum_file_1
+            isupdates = True
+
+        if(memory.get(second_file_path, None) != checksum_file_2):
+            memory[second_file_path] = checksum_file_2
+            isupdates = True
+
+        if(memory.get(third_file_path, None) != checksum_file_3):
+            memory[third_file_path] = checksum_file_3
+            isupdates = True
+        
+        return isupdates
 
     @classmethod
     def _nonblocking_execute_external_code(cls, exe_file_path: str, thread_queue: list):
