0c7d7dc0b975d682ce73e3e5061662e708704920
Utkarsh Jain <b17029@students.iitmandi.ac.in>
2020-11-08 21:11:23 +0530

    Added in-app read/write functionality. Utkarsh.

--- gui_func.py
+++ gui_func.py
@@ -15,7 +15,6 @@ class GUIMain(GUIBase):
 
     @classmethod
     def _refresh_utility(cls, first_file_path: str, second_file_path: str, third_file_path: str, memory: dict) -> bool:
-        
         isupdates = False
         checksum_file_1 = hashlib.sha256(open(first_file_path, 'r').read().encode('utf-8')).hexdigest()
         checksum_file_2 = hashlib.sha256(open(second_file_path, 'r').read().encode('utf-8')).hexdigest()
@@ -52,6 +51,7 @@ class GUIMain(GUIBase):
     def _plot_first_2D_data(cls, output_file_path: str, time_file_path: str):
         time = []
         conc = []
+
         with open(time_file_path,'r') as file:
             data = file.read().splitlines()
             for row in range(1,len(data)):
@@ -75,6 +75,7 @@ class GUIMain(GUIBase):
     def _plot_second_2D_data(cls, output_file_path: str, time_file_path: str):
         time = []
         conc = []
+
         with open(time_file_path,'r') as file:
             data = file.read().splitlines()
             for row in range(1,len(data)):
@@ -114,19 +115,90 @@ class GUIMain(GUIBase):
                 conc1.append(x)
                 conc2.append(y)
 
-
         plt.plot(time,conc1, label='1')
         plt.plot(time,conc2, label='2')
         plt.xlabel('Time')
         plt.ylabel('Concentration')
         plt.title('Concentration-Time graph')
         plt.legend()
-
     
     @classmethod
-    def _inplace_update_variable_dictionary(cls, first_file_path: str, second_file_path: str, third_file_path: str, variable_dictionary: dict) -> None:
-        pass
+    def _inplace_update_variable_dictionary(cls, first_file_path: str, second_file_path: str, third_file_path: str, variable_dictionary: dict) -> None:        
+        try:
+            file1 = open(first_file_path, "r")
+            variable_dictionary["Mesopore seepage velocity"] = file1.readline().strip()
+            variable_dictionary["Macropore seepage velocity"] = file1.readline().strip()
+            variable_dictionary["Solute mass transfer rate b/w meso-micropore"] = file1.readline().strip()
+            variable_dictionary["Solute mass transfer rate b/w meso-macropore"] = file1.readline().strip()
+            variable_dictionary["Dispersivity"] = file1.readline().strip()
+            variable_dictionary["No. of observation time steps"] = file1.readline().strip()
+        finally:
+            file1.close()
+
+        try:
+            file3 = open(third_file_path, "r")
+            line = file3.readline().split()
+            variable_dictionary["nz"] = line[0]
+            variable_dictionary["nm"] = line[1]
+            line = file3.readline().split()
+            variable_dictionary["Length"] = line[0]
+            variable_dictionary["Bulk density of porous media"] = line[1]
+            variable_dictionary["Run time"] = line[2]
+            variable_dictionary["Pulse time"] = line[3]
+            variable_dictionary["delta_t"] = line[4]
+            variable_dictionary["delta_x"] = line[5]
+            line = file3.readline().split()
+            variable_dictionary["Porosity of the macropore region"] = line[0]
+            variable_dictionary["Porosity of the mesopore region"] = line[1]
+            variable_dictionary["Porosity of the micropore region"] = line[2]
+            line = file3.readline().split()
+            variable_dictionary["Instantaneous sorption fraction in macropore region"] = line[0]
+            variable_dictionary["Instantaneous sorption fraction in mesopore region"] = line[1]
+            variable_dictionary["Instantaneous sorption fraction in micropore region"] = line[2]
+            variable_dictionary["Fraction of sorption site available for macropore region"] = line[3]
+            variable_dictionary["Fraction of sorption site available for mesopore region"] = line[4]
+            variable_dictionary["Fraction of sorption site available for immobile region"] = line[5]
+            line = file3.readline().split()
+            variable_dictionary["Equilibrium sorption coefficient in macropore region"] = line[0]
+            variable_dictionary["Equilibrium sorption coefficient in mesopore region"] = line[1]
+            variable_dictionary["Equilibrium sorption coefficient in micropore region"] = line[2]
+            variable_dictionary["Rate-limited sorbed coefficient in macropore region"] = line[3]
+            variable_dictionary["Rate-limited sorbed coefficient in mesopore region"] = line[4]
+            variable_dictionary["Rate-limited sorbed coefficient in micropore region"] = line[5]
+        finally:
+            file3.close()
 
     @classmethod
     def _write_updated_values(cls, first_file_path: str, second_file_path: str, third_file_path: str, variable_dictionary: dict) -> None:
-        pass
+        try:
+            file1 = open(first_file_path, "w")
+            lines = ["Mesopore seepage velocity", "Macropore seepage velocity", "Solute mass transfer rate b/w meso-micropore", "Solute mass transfer rate b/w meso-macropore", "Dispersivity", "No. of observation time steps"]
+            for line in lines:
+                file1.write(variable_dictionary(line)+str("\n"))
+        finally:
+            file1.close()
+
+        try:
+            file3 = open(third_file_path, "w")
+            lines = ["nz", "nm"]
+            for line in lines:
+                file3.write(variable_dictionary(line)+str(" "))
+            file3.write("\n")
+            lines = ["Length", "Bulk density of porous media", "Run time", "Pulse time", "delta_t", "delta_x"]
+            for line in lines:
+                file3.write(variable_dictionary(line)+str(" "))
+            file3.write("\n")
+            lines = ["Porosity of the macropore region", "Porosity of the mesopore region", "Porosity of the micropore region"]
+            for line in lines:
+                file3.write(variable_dictionary(line)+str(" "))
+            file3.write("\n")
+            lines = ["Instantaneous sorption fraction in macropore region", "Instantaneous sorption fraction in mesopore region", "Instantaneous sorption fraction in micropore region", "Fraction of sorption site available for macropore region", "Fraction of sorption site available for mesopore region", "Fraction of sorption site available for immobile region"]
+            for line in lines:
+                file3.write(variable_dictionary(line)+str(" "))
+            file3.write("\n")
+            lines = ["Equilibrium sorption coefficient in macropore region", "Equilibrium sorption coefficient in mesopore region", "Equilibrium sorption coefficient in micropore region", "Rate-limited sorbed coefficient in macropore region", "Rate-limited sorbed coefficient in mesopore region", "Rate-limited sorbed coefficient in micropore region"]
+            for line in lines:
+                file3.write(variable_dictionary(line)+str(" "))
+            file3.write("\n")
+        finally:
+            file3.close()
