16c3c4f6e1768a144142fa4d9e5fe76dcdad0960
Utkarsh Jain <b17029@students.iitmandi.ac.in>
2020-11-08 21:21:37 +0530

    Minor I/O correction. Utkarsh.

--- gui_func.py
+++ gui_func.py
@@ -185,19 +185,19 @@ class GUIMain(GUIBase):
                 file3.write(variable_dictionary(line)+str(" "))
             file3.write(variable_dictionary(lines[-1])+str("\n"))
             lines = ["Length", "Bulk density of porous media", "Run time", "Pulse time", "delta_t", "delta_x"]
-            for line in lines:
+            for line in lines[:-1]:
                 file3.write(variable_dictionary(line)+str(" "))
             file3.write(variable_dictionary(lines[-1])+str("\n"))
             lines = ["Porosity of the macropore region", "Porosity of the mesopore region", "Porosity of the micropore region"]
-            for line in lines:
+            for line in lines[:-1]:
                 file3.write(variable_dictionary(line)+str(" "))
             file3.write(variable_dictionary(lines[-1])+str("\n"))
             lines = ["Instantaneous sorption fraction in macropore region", "Instantaneous sorption fraction in mesopore region", "Instantaneous sorption fraction in micropore region", "Fraction of sorption site available for macropore region", "Fraction of sorption site available for mesopore region", "Fraction of sorption site available for immobile region"]
-            for line in lines:
+            for line in lines[:-1]:
                 file3.write(variable_dictionary(line)+str(" "))
             file3.write(variable_dictionary(lines[-1])+str("\n"))
             lines = ["Equilibrium sorption coefficient in macropore region", "Equilibrium sorption coefficient in mesopore region", "Equilibrium sorption coefficient in micropore region", "Rate-limited sorbed coefficient in macropore region", "Rate-limited sorbed coefficient in mesopore region", "Rate-limited sorbed coefficient in micropore region"]
-            for line in lines:
+            for line in lines[:-1]:
                 file3.write(variable_dictionary(line)+str(" "))
             file3.write(variable_dictionary(lines[-1])+str("\n"))
         finally:
